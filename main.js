e57662bb674eb397d8597879f9285419556ca7e4
Fiorella <fofi-100@hotmail.com>
2021-08-24 11:27:54 -0500

    ejercicios

---
+++ main.js
@@ -0,0 +1,6 @@
+//Ejercicio de colores y número de la suerte
+
+let numero = Math.floor(Math.random()*11);
+let color = prompt("¿Cuál es tu color favorito?");
+alert("Tu color favorito es " + color+ " Y tu número de la suerte es " + numero); 
+
